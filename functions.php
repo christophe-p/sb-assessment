19fda12c6924dc4fe778a1ebbec0d4e74fc54e8d
Christophe <56359946+christophe-p@users.noreply.github.com>
2021-12-14 16:27:51 -0500

    Update functions.php to set up theme settings, enqueue fonts and remove unnecessary code

--- functions.php
+++ functions.php
@@ -34,8 +34,8 @@ if ( ! function_exists( 'azarahealthcare_setup' ) ) :
 		add_theme_support(
 			'custom-logo',
 			array(
-				'height'      => 250,
-				'width'       => 250,
+				'width'       => 137,
+				'height'      => 56,
 				'flex-width'  => true,
 				'flex-height' => true,
 			)
@@ -57,14 +57,15 @@ if ( ! function_exists( 'azarahealthcare_setup' ) ) :
 		);
 
 		// Add custom image size for post thumbnails: 750px wide and unlimited height
-		add_image_size( 'blog-post-thumb', 750, 9999 );
+		//add_image_size( 'blog-post-thumb', 750, 9999 );
 
 		// Register nav menus.
 		register_nav_menus(
       array(
       	'wide-screen-nav' => esc_html__( 'Wide Screen Nav', 'azarahealthcare' ),
  				'mobile-nav' => esc_html__( 'Mobile Nav', 'azarahealthcare' ),
-        'footer-nav' => esc_html__( 'Footer Nav', 'azarahealthcare' ),
+				'footer-nav-1' => esc_html__( 'Footer Nav 1', 'azarahealthcare' ),
+        'footer-nav-1' => esc_html__( 'Footer Nav 2', 'azarahealthcare' ),
       )
    );
 
@@ -75,19 +76,6 @@ add_action( 'after_setup_theme', 'azarahealthcare_setup' );
 
 // Register widget areas
 function azarahealthcare_widgets_init() {
-
-	// Main Sidebar Widget Area
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'azarahealthcare' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add sidebar widgets here.', 'azarahealthcare' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h3 class="sidebar-widget-title">',
-			'after_title'   => '</h3>',
-		)
-	);
 
 	// Footer Widget Areas
 	register_sidebar( array(
@@ -130,62 +118,27 @@ add_action( 'widgets_init', 'azarahealthcare_widgets_init' );
 function azarahealthcare_enqueue_scripts() {
 
 	// Enqueue Google Fonts
-	wp_enqueue_style( 'google-font-montserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', array(), '', 'all' );
+	wp_enqueue_style( 'google-font-roboto', 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap', array(), false, 'all' );
 
-	wp_enqueue_style( 'google-font-open-sans', 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap', array(), '', 'all' );
-
-	//wp_enqueue_style( 'google-font-varela', 'https://fonts.googleapis.com/css2?family=Varela&display=swap', array(), '', 'all' );
-
-	wp_enqueue_style( 'google-font-roboto', 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap', array(), '', 'all' );
-
-	wp_enqueue_style( 'google-font-source-serif-pro', 'https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap', array(), '', 'all' );
+	wp_enqueue_style( 'google-font-quicksand', 'https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap', array(), false, 'all' );
 
 	// Enqueue dashicons
 	wp_enqueue_style('dashicons');
 
 	// Enqueue Font Awesome
-	wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css', array(), '', 'all' );
-
-	// Enqueue Bootstrap 4
-	//wp_enqueue_style( 'bootstrap4', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css', array(), '', 'all' );
-
-	// Enqueue Animate.css
-	wp_enqueue_style( 'animate-css', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.0/animate.min.css', array(), '', 'all' );
+	wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css', array(), false, 'all' );
 
 	// Enqueue main style sheet with dependencies
-	wp_enqueue_style( 'azarahealthcare-style', get_stylesheet_uri(), array( 'google-font-montserrat', 'google-font-open-sans', 'google-font-roboto' , 'google-font-source-serif-pro' ), _S_VERSION );
-
-	// Underscores stuff
-	wp_style_add_data( 'azarahealthcare-style', 'rtl', 'replace' );
+	wp_enqueue_style( 'azarahealthcare-style', get_stylesheet_uri(), array( 'google-font-roboto' , 'google-font-quicksand' ), false, 'all' );
 
 	// Enqueue hamburger JS
 	wp_enqueue_script( 'hamburger', get_template_directory_uri() . '/js/hamburger.js', array(), _S_VERSION, true );
-
-	// Enqueue accordion JS
-	wp_enqueue_script( 'accord', get_template_directory_uri() . '/js/accord.js', array(), _S_VERSION, true );
-
-	// Enqueue back-to-top JS
-	wp_enqueue_script( 'back-to-top', get_template_directory_uri() . '/js/back-to-top.js', array(), _S_VERSION, true );
-
-	// Enqueue animated logo JS
-	//wp_enqueue_script( 'logo', get_template_directory_uri() . '/js/logo.js', array(), _S_VERSION, true );
-
-	// Enqueue Bootstrap JS with jQuery as a dependency
-	//wp_enqueue_script( 'bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array( 'jquery' ), _S_VERSION, true );
-
-	// Enqueue custom Bootstrap JS with jQuery as a dependency
-	//wp_enqueue_script( 'custom-bootstrap-js', get_template_directory_uri() . '/js/bootstrap.js', array( 'jquery' ), _S_VERSION, true );
-
-	// Enqueue Popper JS library
-	//wp_enqueue_script( 'popper-js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'azarahealthcare_enqueue_scripts' );
-// Enqueue the same scripts and styles on the back end
-//add_action('admin_enqueue_scripts', 'azarahealthcare_enqueue_scripts');
 
 /**
  * Load comment form and comments
@@ -214,44 +167,13 @@ require get_template_directory() . '/inc/theme/read-more.php';
 require get_template_directory() . '/inc/theme/archive-titles.php';
 
 /**
- * Enable Smart Nav One, which changes the top nav color when the user scrolls down
- */
-//require get_template_directory() . '/inc/navigation/smart-nav-one.php';
-
-/**
- * Hide specific dashboard menus for non-admin users
- */
-//require get_template_directory() . '/inc/admin/hide-menus.php';
-
-/**
  * Customizer additions
  */
 require get_template_directory() . '/inc/admin/customizer.php';
 
 /**
- * Custom ACF blocks
- */
-require get_template_directory() . '/inc/acf/blocks.php';
-
-
-/**
 	* Underscores default includes
 	*/
-
-/**
- * Implement the Custom Header feature.
- */
-//require get_template_directory() . '/inc/underscores/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/underscores/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/underscores/template-functions.php';
 
 /**
  * Load Jetpack compatibility file.
