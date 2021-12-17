5f3ec71f2fc77bb0ee57e577324d3e3a119ad32c
Christophe <56359946+christophe-p@users.noreply.github.com>
2021-12-16 21:14:08 -0500

    Create template part for home page's section 2

---
+++ template-parts/front-page-section-2.php
@@ -0,0 +1,57 @@
+<!-- Section 2 -->
+
+<section id="hp-section-2" class="hp-section-2">
+
+  <section class="hp-cards-wrapper">
+
+    <section class="hp-card">
+
+     <div class="hp-card-icon">
+       <img src="https://sb-assessment.local/wp-content/uploads/2021/12/document.svg" alt="">
+     </div>
+
+     <h3 class="hp-card-heading">
+       <?php the_field( 'hp_section_2_card_1_heading' ); ?>
+     </h3>
+
+     <p class="hp-card-text">
+      <?php the_field( 'hp_section_2_card_1_text' ); ?>
+     </p>
+
+    </section>
+
+    <section class="hp-card">
+
+     <div class="hp-card-icon">
+      <img src="https://sb-assessment.local/wp-content/uploads/2021/12/restricted.svg" alt="">
+     </div>
+
+     <h3 class="hp-card-heading">
+       <?php the_field( 'hp_section_2_card_2_heading' ); ?>
+     </h3>
+
+     <p class="hp-card-text">
+      <?php the_field( 'hp_section_2_card_2_text' ); ?>
+     </p>
+
+    </section>
+
+    <section class="hp-card">
+
+     <div class="hp-card-icon">
+       <img src="https://sb-assessment.local/wp-content/uploads/2021/12/family.svg" alt="">
+     </div>
+
+     <h3 class="hp-card-heading">
+       <?php the_field( 'hp_section_2_card_3_heading' ); ?>
+     </h3>
+
+     <p class="hp-card-text">
+      <?php the_field( 'hp_section_2_card_3_text' ); ?>
+     </p>
+
+    </section>
+
+  </section>
+
+</section>
