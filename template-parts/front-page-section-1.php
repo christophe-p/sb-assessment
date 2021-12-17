c00c98d1ed710ec8c55adc08b77fff74ef7dfc41
Christophe <56359946+christophe-p@users.noreply.github.com>
2021-12-16 21:14:01 -0500

    Create template part for home page's section 1

---
+++ template-parts/front-page-section-1.php
@@ -0,0 +1,17 @@
+<!-- Section 1 -->
+
+<section id="hp-section-1" class="hp-section-1">
+
+  <section class="hp-section-1-image">
+      <img src="https://sb-assessment.local/wp-content/uploads/2021/12/case-workers.png" alt="">
+  </section>
+
+  <section class="hp-section-1-text">
+
+    <h2><?php the_field( 'hp_section_1_heading' ); ?></h2>
+
+    <p><?php the_field( 'hp_section_1_text' ); ?></p>
+
+  </section>
+
+</section>
