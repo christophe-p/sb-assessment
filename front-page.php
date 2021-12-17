566c9072cf136ca97b11e883d1b323d8cb157845
Christophe <56359946+christophe-p@users.noreply.github.com>
2021-12-17 11:29:30 -0500

    Create ans style section 5

--- front-page.php
+++ front-page.php
@@ -8,5 +8,7 @@
 
   <?php locate_template( 'template-parts/front-page-section-4.php', true ); ?>
 
+  <?php locate_template( 'template-parts/front-page-section-5.php', true ); ?>
+
 <?php
  get_footer();
---
+++ template-parts/front-page-section-5.php
@@ -0,0 +1,57 @@
+<!-- Section 5 -->
+
+<section id="hp-section-5" class="hp-section-5">
+
+  <section class="hp-cards-wrapper">
+
+    <section class="hp-card">
+
+     <div class="hp-card-icon">
+       <img src="https://sb-assessment.local/wp-content/uploads/2021/12/progress.svg" alt="">
+     </div>
+
+     <h3 class="hp-card-heading">
+       <?php the_field( 'hp_section_5_card_1_heading' ); ?>
+     </h3>
+
+     <p class="hp-card-text">
+      <?php the_field( 'hp_section_5_card_1_text' ); ?>
+     </p>
+
+    </section>
+
+    <section class="hp-card">
+
+     <div class="hp-card-icon">
+      <img src="https://sb-assessment.local/wp-content/uploads/2021/12/care.svg" alt="">
+     </div>
+
+     <h3 class="hp-card-heading">
+       <?php the_field( 'hp_section_5_card_2_heading' ); ?>
+     </h3>
+
+     <p class="hp-card-text">
+      <?php the_field( 'hp_section_5_card_2_text' ); ?>
+     </p>
+
+    </section>
+
+    <section class="hp-card">
+
+     <div class="hp-card-icon">
+       <img src="https://sb-assessment.local/wp-content/uploads/2021/12/happy-family.svg" alt="">
+     </div>
+
+     <h3 class="hp-card-heading">
+       <?php the_field( 'hp_section_5_card_3_heading' ); ?>
+     </h3>
+
+     <p class="hp-card-text">
+      <?php the_field( 'hp_section_5_card_3_text' ); ?>
+     </p>
+
+    </section>
+
+  </section>
+
+</section>
