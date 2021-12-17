2317ae10ddd87a2407c58aa60951412be511afe3
Christophe <56359946+christophe-p@users.noreply.github.com>
2021-12-16 22:06:17 -0500

    Create and style sections 1, 2 and 3

--- front-page.php
+++ front-page.php
@@ -1,5 +1,11 @@
 <?php get_header(); ?>
 
+  <?php locate_template( 'template-parts/front-page-section-1.php', true ); ?>
+
+  <?php locate_template( 'template-parts/front-page-section-2.php', true ); ?>
+
+  <?php locate_template( 'template-parts/front-page-section-3.php', true ); ?>
+
   <!-- ?php locate_template( 'template-parts/front-page-services.php', true ); ? -->
 
   <!-- ?php locate_template( 'template-parts/front-page-about.php', true ); ? -->
---
+++ template-parts/front-page-section-3.php
@@ -0,0 +1,13 @@
+<!-- Section 3 -->
+
+<section id="hp-section-3" class="hp-section-3">
+
+  <section class="hp-section-3-text">
+
+    <h2><?php the_field( 'hp_section_3_heading' ); ?></h2>
+
+    <p><?php the_field( 'hp_section_3_text' ); ?></p>
+
+  </section>
+
+</section>
